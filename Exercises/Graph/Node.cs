91fe803c5e41217eb8d1625edbd182e8ba683697
Jan-SverreRiksfjord <jasverix@gmail.com>
2022-05-12 14:46:55 +0200

    change name to cost

--- Exercises/Graph/Node.cs
+++ Exercises/Graph/Node.cs
@@ -11,23 +11,23 @@ public class Node {
     private const double Unreachable = double.PositiveInfinity;
     private readonly List<Edge> _edges = new List<Edge>();
 
-    public bool CanReach(Node destination) => PathValue(destination, NoVisitedNodes(), Edge.HopStrategy) != Unreachable;
+    public bool CanReach(Node destination) => Cost(destination, NoVisitedNodes(), Edge.HopStrategy) != Unreachable;
 
-    public int HopCount(Node destination) => (int)PathValue(destination, Edge.HopStrategy);
+    public int HopCount(Node destination) => (int)Cost(destination, Edge.HopStrategy);
 
-    public double MinCost(Node destination) => PathValue(destination, Edge.CostStrategy);
+    public double MinCost(Node destination) => Cost(destination, Edge.CostStrategy);
 
-    private double PathValue(Node destination, Func<double, double> strategy) {
-        var result = PathValue(destination, NoVisitedNodes(), strategy);
+    private double Cost(Node destination, Func<double, double> strategy) {
+        var result = Cost(destination, NoVisitedNodes(), strategy);
         if (result == Unreachable) throw new ArgumentException("Destination cannot be reached");
         return result;
     }
 
-    private double PathValue(Node destination, List<Node> visitedNodes, Func<double, double> strategy) {
+    private double Cost(Node destination, List<Node> visitedNodes, Func<double, double> strategy) {
         if (this == destination) return 0.0;
         if (visitedNodes.Contains(this)) return Unreachable;
         if (_edges.Count == 0) return Unreachable;
-        return _edges.Min(e => e.PathValue(destination, CopyWithThis(visitedNodes), strategy));
+        return _edges.Min(e => e.Cost(destination, CopyWithThis(visitedNodes), strategy));
     }
 
     private List<Node> CopyWithThis(List<Node> originals) => new List<Node>(originals) {this};
@@ -46,8 +46,8 @@ public class Node {
             _cost = cost;
         }
 
-        internal double PathValue(Node destination, List<Node> visitedNodes, Func<double, double> strategy)
-            => strategy(this._cost) + _node.PathValue(destination, visitedNodes, strategy);
+        internal double Cost(Node destination, List<Node> visitedNodes, Func<double, double> strategy)
+            => strategy(this._cost) + _node.Cost(destination, visitedNodes, strategy);
     }
 
     public EdgeBuilder Cost(double amount) => new EdgeBuilder(amount, _edges);
